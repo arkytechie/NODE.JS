6fabeb05bed2af6b8298bceb613e74e10e0999bc
arkytechie <40334092+arkytechie@users.noreply.github.com>
2021-04-30 23:23:43 +0200

    initial hands on

---
+++ app.js
@@ -0,0 +1,7 @@
+/*
+const tutorial = require ('./tutorial');
+console.log(tutorial.sum(2,3));
+console.log(tutorial.PI);
+console.log(new tutorial.someMathObject());
+//console.log(tutorial(1,1));
+*/
